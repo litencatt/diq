cae57fd1bdfee7de812da433e9839394cc211f9f
litencatt <ncl0709@gmail.com>
2021-02-13 19:44:24 +0900

    Scaffold strings

--- cmd/root.go
+++ cmd/root.go
@@ -62,14 +62,7 @@ var domainNames []string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "diq",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:  "diq",
 	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		setDomainNames(args)
